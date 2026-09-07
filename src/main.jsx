f129eed37484893227fa13e930ea1f5a57f345a3
arjun123970 <arjunmajety07@gmail.com>
2026-09-07 10:39:09 -0400

    updated sops link

--- src/main.jsx
+++ src/main.jsx
@@ -111,7 +111,7 @@ const td0ChecklistItems = [
 ]
 
 const uniformPdf = "https://static.e-publishing.af.mil/production/1/af_a1/publication/dafi36-2903/dafi36-2903.pdf"
-const det220SopsUrl = "https://purdue0.sharepoint.com/sites/AFROTCDetachment220/Shared%20Documents/General/F26%20SOPs%20-%20CAO%2021%20Aug.pdf?TeamsCID=be777d09-6c25-40da-b90b-a77c6971f10e"
+const det220SopsUrl = "https://purdue0.sharepoint.com/:b:/r/sites/AFROTCDetachment220/Shared%20Documents/General/01.%20Det%20220%20Policy%20and%20Reference%20Docs/2.%20F26%20SOPs%20-%20CAO21Aug26.pdf?d=wd5a9c815ce2841ed87144caeefd38b79&csf=1&web=1&e=ce556c"
 const weeklyOpordUrl = "https://purdue0.sharepoint.com/:b:/r/sites/AFROTCDetachment220/Shared%20Documents/General/F26%20OPORD%202.pdf?d=w382fdd0f11d84459add8b2f3f381712b&csf=1&web=1&e=ikldxw"
 const cadetShoutoutFormUrl = "https://forms.cloud.microsoft/r/L2RpnjAX5R"
 
